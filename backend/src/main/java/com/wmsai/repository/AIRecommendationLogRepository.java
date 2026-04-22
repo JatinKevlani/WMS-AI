32f0d5d5d2161d09905b801d3ac24faeaa86e911
JatinKevlani <jatinkevlani01@gmail.com>
2026-04-22 09:16:03 +0530

    REMOVE: unnecessary files

--- backend/src/main/java/com/wmsai/repository/AIRecommendationLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wmsai.repository;
-
-import com.wmsai.entity.AIRecommendationLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AIRecommendationLogRepository extends JpaRepository<AIRecommendationLog, Integer> {
-}
--- backend/src/main/java/com/wmsai/repository/ProductBundleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wmsai.repository;
-
-import com.wmsai.entity.ProductBundle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProductBundleRepository extends JpaRepository<ProductBundle, Integer> {
-    List<ProductBundle> findByProductId(Integer productId);
-}
